b10532a768168724740693dde960d4caa73c9a0d
Nicole Rauch <nicole.m@gmx.de>
2016-08-27 19:06:35 +0200

    A smarter solution for Problem 5.

--- Haskell/5_multiple.hs
+++ Haskell/5_multiple.hs
@@ -12,7 +12,33 @@ multiple maxnum =
   in [x | x <- candidates, all (\i -> x `rem` i == 0) nonps]
 
 
+ffactors :: Int -> [Int]
+ffactors x = factors x primes
+  where
+    factors x (p:ps)
+      | x < p          = []
+      | x `rem` p == 0 = p : (factors (x `div` p) (p:ps))
+      | otherwise      = factors x ps
+
+
+filterFactors [] fs = fs
+filterFactors (x:xs) fs =
+  let
+    removeFrom [] newL = newL
+    removeFrom (o:os) newL
+      | elem o newL = removeFrom os (newL \\ [o])
+      | otherwise = removeFrom os newL
+    addUnique oldL newL = oldL ++ removeFrom oldL newL
+  in filterFactors [y | y <- xs, x `rem` y /= 0] $ addUnique fs (ffactors x)
+
+-- try a different strategy:
+multiple2 :: Int -> Int
+multiple2 maxnum =
+  let numbers = filterFactors [maxnum,(maxnum-1)..1] []
+  in foldr (\i j -> i * j) 1 numbers
+
 main = do
   putStrLn $ show $ head $ multiple 10
-  putStrLn $ show $ head $ multiple 20
+  putStrLn $ show $ multiple2 20
+  -- putStrLn $ show $ head $ multiple 20
 
