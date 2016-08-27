5847d4ac3ad4e7447831a8527825698f472ad6ce
Nicole Rauch <nicole.m@gmx.de>
2016-08-27 18:33:47 +0200

    A first but rather slow solution for Problem 5.

--- Haskell/5_multiple.hs
+++ Haskell/5_multiple.hs
@@ -1,7 +1,16 @@
 module M5 where
 
+import Primes
+import Data.List ((\\))
+
+-- works but is rather slow:
 multiple :: Int -> [Int]
-multiple maxnum = [x | x <- [1..], all (\i -> x `rem` i == 0) [1..maxnum]]
+multiple maxnum =
+  let ps = (takeWhile (<= maxnum) primes)
+      nonps = [1..maxnum] \\ ps
+      candidates = filter (\x -> all (\i -> x `rem` i == 0) ps) [1..]
+  in [x | x <- candidates, all (\i -> x `rem` i == 0) nonps]
+
 
 main = do
   putStrLn $ show $ head $ multiple 10
